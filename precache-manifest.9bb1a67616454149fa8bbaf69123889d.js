8afa7e422bc625f7a3f620cba4e5184b8819108f
goblincore <donny@Donnys-MBP.fios-router.home>
2019-03-15 01:16:29 -0400

    Updates

---
+++ precache-manifest.9bb1a67616454149fa8bbaf69123889d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04baee07d625bbf3d5d9",
+    "url": "//articulatetest/static/js/runtime~main.70850da4.js"
+  },
+  {
+    "revision": "3589b3029cbc4d4eb762",
+    "url": "//articulatetest/static/js/main.dcdd5c24.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "//articulatetest/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "3589b3029cbc4d4eb762",
+    "url": "//articulatetest/static/css/main.f71c6f23.chunk.css"
+  },
+  {
+    "revision": "5ab272ab5983b32d36bcf96c4466c986",
+    "url": "//articulatetest/index.html"
+  }
+];
